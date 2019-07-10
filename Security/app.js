5a615c13312f70c87e1b29e1678ffabbdb361125
huybkdn23 <huybkdn23@gmail.com>
2019-07-10 11:36:07 +0700

    Security

--- Security/app.js
+++ Security/app.js
@@ -7,6 +7,7 @@ const csurf         = require("csurf");
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", "./views");
+app.disable("x-powered-by");
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
