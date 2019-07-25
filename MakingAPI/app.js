68f2c421eb983fdf9253dde1c7de060ae379794f
huybkdn23 <huybkdn23@gmail.com>
2019-07-26 01:13:25 +0700

    Swagger UI Express

--- MakingAPI/app.js
+++ MakingAPI/app.js
@@ -4,6 +4,8 @@ const bodyParser    = require('body-parser');
 const dotenv        = require('dotenv');
 const connect_db    = require('./configs/db.js');
 const api           = require('./routes/index.js');
+const swaggerUI     = require('swagger-ui-express');
+const swaggerDoc    = require('./swagger.json');
 const app           = express();
 dotenv.config();
 const PORT = process.env.HOST_PORT || 8000;
@@ -14,6 +16,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 app.use('/v1', api);
 
 app.listen(PORT, () => {
